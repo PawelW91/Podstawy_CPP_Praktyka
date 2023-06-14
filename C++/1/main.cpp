5f6b4f1b459ad205ccab28779284fb206b980382
Paweł Wachol <wachol1991@o2.pl>
2023-06-14 20:21:45 +0200

    v1.4

--- C++/1/main.cpp
+++ C++/1/main.cpp
@@ -9,66 +9,26 @@
 #include <algorithm>
 #include <string>
 
-
 using namespace std;
-
-string temat,nick;
-string pytanie[5];
-string A[5],B[5],C[5],D[5];
-string OK[5];
-string odp;
-int pkt=0;
 
 
 int main()
 {
-    int Nr=1;
-    string L;
-    int NrP=0;
-
-    fstream plik;
-    plik.open("quiz.txt",ios::in);
-
-    if(plik.good()==false){cout<<"Plik nie istnieje ;-("; exit(0);}
-
-    while(getline(plik,L))
+    string I,P,O;
+    int L;
+    cout<<"Jak masz na imie? ";
+    getline(cin,I);
+    getline(cin,P);
+    O=I+P;
+    L=O.length();
+    for (int i=0; i<L/2; i++)
     {
-        switch(Nr)
-        {
-            case 1: temat=L;            break;
-            case 2: nick=L;             break;
-            case 3: pytanie[NrP]=L;     break;
-            case 4: A[NrP]=L;           break;
-            case 5: B[NrP]=L;           break;
-            case 6: C[NrP]=L;           break;
-            case 7: D[NrP]=L;           break;
-            case 8: OK[NrP]=L;          break;
-        }
-        if(Nr==8){Nr=2; NrP++;}
-        Nr++;
+        char B;
+        B=O[i];
+        O[i]=O[L-1-i];
+        O[L-1-i]=B;
     }
-    plik.close();
-
-        cout<<temat<<endl<<nick<<endl;
-    for (int i=0; i<=4; i++)
-    {
-        cout<<endl<<pytanie[i]<<endl;
-        cout<<"A. "<<A[i]<<endl;
-        cout<<"B. "<<B[i]<<endl;
-        cout<<"C. "<<C[i]<<endl;
-        cout<<"D. "<<D[i]<<endl;
-        cout<<"Twoja odpowiedz: ";
-        cin>>odp;
-        transform(odp.begin(),odp.end(),odp.begin(),::tolower);
-
-        if(odp==OK[i])
-        {
-            cout<<"Dobrze! Zdobywasz pkt!"<<endl;
-            pkt++;
-        }else cout<<"Zle! Brak pkt! Poprawna odp to: "<<OK[i]<<endl;
-    }
-    cout<<"Koniec! Zdobyles: "<<pkt;
-
+    cout<<O;
 
     return 0 ;
 }
