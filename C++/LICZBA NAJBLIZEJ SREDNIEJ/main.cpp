97eca60840a2900364dff7c9a95aa7f48553d05e
Paweł Wachol <wachol1991@o2.pl>
2023-06-22 20:41:24 +0200

    22.06.2023

--- C++/LICZBA NAJBLIZEJ SREDNIEJ/main.cpp
+++ C++/LICZBA NAJBLIZEJ SREDNIEJ/main.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
-#include <cmath>
+#include <windows.h>
+#include <cstdlib>
+#include <time.h>
+#include <stdio.h>
+#include <iomanip>
+#include <conio.h>
+#include <fstream>
+#include <algorithm>
+#include <string>
+
 using namespace std;
 
-int ile_t,ile_l,suma;
-float sr;
+
 
 int main()
 {
-    cin>>ile_t;
-    cout<<endl;
-for (int i=0;i<ile_t;i++)
-{
-    cin>>ile_l;
-    int liczby[ile_l];
-    float zblizona[ile_l];
-    suma=0;
-    for (int i=0;i<ile_l;i++)
+    float s=0,T[5],m;
+    cout<<"Wymien 5 liczb: ";
+    for(int i=0;i<5;i++){cin>>T[i];}
+    m=T[0];
+        for(int i=0;i<5;i++)
+        {
+            s+=T[i];
+        }
+        s=s/5;
+
+
+        cout<<"Srednia to: "<<s<<endl;
+
+    for(int i=1;i<5;i++)
     {
-        cin>>liczby[i];
-        suma+=liczby[i];
+        if((abs(s-m))>(abs(s-T[i])))
+           {m=T[i];}
+           cout<<m<<endl;
     }
-    sr=suma/ile_l;
+
+    cout<<"Najblizej sredniej jest: "<<m;
 
 
 
-
-
-
-
-    cout<<endl<<sr<<endl<<endl;
-
-}
-
-    return 0;
+    return 0 ;
 }
