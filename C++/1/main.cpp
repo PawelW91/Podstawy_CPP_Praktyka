83a52eb174a1288402619bb5cd300d50e7961abd
Paweł Wachol <wachol1991@o2.pl>
2023-06-26 18:29:57 +0200

    26.06.2023

--- C++/1/main.cpp
+++ C++/1/main.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
-int X (int a,int b){
-    while(a!=b){
-            if (a>b)    {a-=b;}
-            else        {b-=a;}
-            }
-            return a+b;
-    }
 int main()
 {
     int ile_t;
-    int wynik[ile_t];
-    cin>>ile_t;
-    for(int i=0;i<ile_t;i++)
-    {
-        int a,b;
-        cin>>a>>b;
-        wynik[i]=X(a,b);
+    cin >> ile_t;
+
+    while (ile_t--) {
+        double A, B, C, D, A1, B1, C1, D1;
+        cin >> A1 >> B1 >> C1 >> D1;
+
+        A = max(A1, B1);
+        B = min(A1, B1);
+        C = max(C1, D1);
+        D = min(C1, D1);
+
+
+                if(((A>C)&&(B>D))||((A>D)&&(B>C))){
+                    cout << "TAK" << endl;
+                }
+                    else{
+                        if(((A<=C)&&(B<=D))||((A<=D)&&(B<=C))){
+                            cout << "NIE" << endl;
+                        }
+                        else{
+                                double d1,d2;
+                                d1=sqrt(pow(A,2)+pow(B,2));
+                                d2=sqrt(pow(C,2)+pow(D,2));
+
+                                if (d1<=d2){cout<<"NIE"<<endl;}
+                                else{
+                                        double a,b,c;
+                                        a = 0.5*A-sqrt(pow(0.5*d2,2)-pow(0.5*B,2));
+                                        b = 0.5*B-sqrt(pow(0.5*d2,2)-pow(0.5*A,2));
+                                        c = sqrt(a*a+b*b);
+
+                                        if(c>D) cout<<"TAK"<<endl;
+                                        else cout<<"NIE"<<endl;
+                                }
+                        }
+                    }
     }
 
-    for (int i=0;i<ile_t;i++) {
-        cout<<wynik[i]<<endl;
-    }
-
-    return 0 ;
+    return 0;
 }
+
+
+
+
+//x=(D*(C*(sqrt(pow(C,2)+pow(D,2)-pow(B,2))-B*D)))/(pow(D,2)-pow(C,2));
+//x=-(D*(C*(sqrt(pow(C,2)+pow(D,2)-pow(B,2))+B*D)))/(pow(D,2)-pow(C,2));
+
+
+
+
