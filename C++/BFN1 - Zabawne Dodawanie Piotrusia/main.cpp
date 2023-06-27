85f1f01945edf1c6a4b3d69c49363fa56128ae67
Paweł Wachol <wachol1991@o2.pl>
2023-06-27 20:43:20 +0200

    27.06.2023

--- C++/BFN1 - Zabawne Dodawanie Piotrusia/main.cpp
+++ C++/BFN1 - Zabawne Dodawanie Piotrusia/main.cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
+int odwracanie_liczby (int X){int X1=0; while (X!=0) {X1=X1*10+X%10;X/=10;} return X1;} //Zapis liczby od tylu.
+
 int main()
 {
     int ile_t;
     cin>>ile_t;
     while (ile_t--)
     {
-        int X,Y,X1=0,ile=0;
+        int X,X1,ile=0;
         cin>>X;
-        Y=X;
-        while (Y!=0) {X1=X1*10+Y%10;Y/=10;}
+        X1=odwracanie_liczby(X);
+
         if (X==X1){cout<<X<<" "<<ile<<endl;}
             else
                 {
                     while(!(X==X1))
                     {
                         ile++;
-                        X=X+X1;
-                        Y=X;
-                        X1=0;
-                        while (Y!=0) {X1=X1*10+Y%10;Y/=10;}
+                        X+=X1;
+                        X1=odwracanie_liczby(X);
                     }
                     cout<<X<<" "<<ile<<endl;
                     ile=0;
