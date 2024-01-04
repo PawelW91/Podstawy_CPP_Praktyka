692dcacb35a148785f67cba6a3dc31fd3f70f455
Paweł Wachol <wachol1991@o2.pl>
2024-01-04 16:52:53 +0100

    04.01.2024

--- Zelent/Wskazniki/main.cpp
+++ Zelent/Wskazniki/main.cpp
@@ -14,42 +14,15 @@ int main()
 
     cout<<"Ile: ";
     cin>>ile;
-
-
-
-    int *tablica;
-    tablica = new int [ile];
-
-
-
-    start=clock();
-    for(int i=0;i<ile;i++)
-    {
-        tablica[i]=i;
-        tablica[i]+=50;
-    }
-    stop=clock();
-    czas=(double)(stop-start)/CLOCKS_PER_SEC;
-    cout<<"Czas bez wskaznika: "<<czas<<endl;
-    delete [] tablica;
-
-
-
-    start=clock();
     int *w;
-    w=new int [ile];
+    w=new int;
     for(int i=0;i<ile;i++)
     {
-        *w=i;
-        *w+=50;
+
+        cout<<(int)w<<endl;
         w++;
     }
-    stop=clock();
-    czas=(double)(stop-start)/CLOCKS_PER_SEC;
-    cout<<"Czas ze wskaznikiem: "<<czas<<endl;
-    delete [] tablica;
-
-
+    delete[] w;
 
     return 0;
 }
