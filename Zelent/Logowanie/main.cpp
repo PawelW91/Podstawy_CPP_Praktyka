686cbabe4dda78e9ab92f46bf23c5d1d17643482
Paweł Wachol <wachol1991@o2.pl>
2024-01-05 22:14:43 +0100

    05.01.2024

--- Zelent/Logowanie/main.cpp
+++ Zelent/Logowanie/main.cpp
@@ -2,6 +2,71 @@
 
 using namespace std;
 
+
+int f(int n)
+{
+    if(n==0)return 3;
+    else return f(n-1)+2;
+}
+
+long int potega(int p,int w)
+{
+        if(w==1) return p;
+        else return p*potega(p,w-1);
+}
+long int fib (int n)
+{
+    if(n==1||n==2) return 1;
+    else return fib(n-1)+fib(n-2);
+}
+long int silnia(int x)
+{
+    if(x==1) return 1;
+    else return x*silnia(x-1);
+}
+int main()
+{
+    cout<<silnia(5)<<endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+using namespace std;
+
 int main()
 {
     int a,b;
@@ -12,7 +77,7 @@ int main()
 
     return 0;
 }
-
+*/
 
 
 
