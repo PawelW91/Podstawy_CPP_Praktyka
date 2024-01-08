9376a9354d83bebe2f2d31532dc8822811cc7168
Paweł Wachol <123093109+PawelW91@users.noreply.github.com>
2024-01-08 19:55:41 +0100

    Update main.cpp

--- Polski_Spoj/AL_11_07 - Enigma w wersji light/main.cpp
+++ Polski_Spoj/AL_11_07 - Enigma w wersji light/main.cpp
@@ -10,12 +10,12 @@ int main()
         if(x.length()<2) cout<<x;
         else if (x.length()>=2)
         {
-            // Zamieniam miejscami s¹siednie znaki.
+            // Zamieniam miejscami sÄ…siednie znaki.
             for(int i=0;i<x.length()-1;i+=2)
             {
                 cout<<x[i+1]<<x[i];
             }
-            // Jeœli d³ugoœæ ci¹gu jest nieparzysta, wypisz ostatni znak.
+            // JeÅ›li dÅ‚ugoÅ›Ä‡ ciÄ…gu jest nieparzysta, wypisz ostatni znak.
             if (x.length()%2!=0)
             {
                 cout<<x[x.length()-1];
