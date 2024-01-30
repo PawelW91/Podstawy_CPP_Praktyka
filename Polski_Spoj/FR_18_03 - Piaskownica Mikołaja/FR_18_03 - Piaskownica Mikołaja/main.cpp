16c06a8d8f0f463366d38b30d7dc205cd0bd1a11
Paweł Wachol <wachol1991@o2.pl>
2024-01-30 20:29:02 +0100

    30.01.2024

---
+++ Polski_Spoj/FR_18_03 - Piaskownica Mikołaja/FR_18_03 - Piaskownica Mikołaja/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+
+    int y[100];
+
+    for(int i=0;i<100;i++)
+    {
+        cout<<y[i]<<endl;
+    }
+
+
+    return 0;
+}
