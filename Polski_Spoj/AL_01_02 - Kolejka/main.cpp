122b3e37ff3c0bc3f0883d03f403a96b2077d8d6
Paweł Wachol <wachol1991@o2.pl>
2024-01-30 10:35:55 +0100

    30.01.2024

--- Polski_Spoj/AL_01_02 - Kolejka/main.cpp
+++ Polski_Spoj/AL_01_02 - Kolejka/main.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
-#include <deque>
+#include <string>
 
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);  // Faster input/output
+
     int ile;
     cin >> ile;
 
     while (ile--) {
-        string x;
+        string x, y;
         cin >> x;
+        y.reserve(x.length());  // Reserve memory for y
+        y.push_back(x.back());
+        char lider = x.back();
 
-        deque<char> result;
-
-        for (char c : x) {
-            while (!result.empty() && c > result.back()) {
-                result.pop_back();
+        for (int i = x.length() - 1; i >= 0; i--) {
+            if (lider <= x[i - 1]) {
+                lider = x[i - 1];
+                y.push_back(lider);
             }
-            result.push_back(c);
         }
 
-        for (char c : result) {
-            cout << c;
+        for (int i = y.length() - 1; i >= 0; i--) {
+            cout << y[i];
         }
 
-        cout << endl;
+        cout << '\n';  // Use '\n' instead of endl
     }
 
     return 0;
